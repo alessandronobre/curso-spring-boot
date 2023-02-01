43bdde9291aeee482a244d503f7d8be5ca4d8d06
Alessandro <alessandronobre.ti@gmail.com>
2023-01-31 23:36:43 -0300

    User update

--- src/main/java/br/com/cursospring/services/UserService.java
+++ src/main/java/br/com/cursospring/services/UserService.java
@@ -37,4 +37,20 @@ public class UserService {
 //			throw new DatabaseException(e.getMessage());
 //		}
 	}
+	
+	public User update(Long id, User obj) {
+//		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+//		} catch (EntityNotFoundException e) {
+//			throw new ResourceNotFoundException(id);
+//		}	
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
